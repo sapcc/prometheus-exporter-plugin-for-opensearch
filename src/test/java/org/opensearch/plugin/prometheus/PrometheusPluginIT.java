830f0f15e415606cba05e205c77aafecfd952bcb
Olaf Heydorn <olaf.heydorn@sap.com>
2025-07-11 12:08:20 +0200

    adding class to PrometheusPluginIT

--- src/test/java/org/opensearch/plugin/prometheus/PrometheusPluginIT.java
+++ src/test/java/org/opensearch/plugin/prometheus/PrometheusPluginIT.java
@@ -24,6 +24,7 @@ import org.opensearch.client.Request;
 import org.opensearch.client.Response;
 import org.opensearch.client.RestClient;
 import org.opensearch.plugins.Plugin;
+import org.opensearch.test.OpenSearchTestCase
 import org.opensearch.test.OpenSearchIntegTestCase;
 
 import java.io.IOException;
